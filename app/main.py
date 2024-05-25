b926111a0433bd3bbc8143523f759db4a9c2d016
nissen <nishan.yeh@junyiacademy.org>
2024-05-26 01:40:04 +0800

    use threading

--- app/main.py
+++ app/main.py
@@ -6,6 +6,8 @@ import threading
 def handle_connection(client_socket):
     while True:
         request: bytes = client_socket.recv(1024) # 獲取客戶端發送的訊息
+        if not request:
+            break;
         message: str = request.decode()
         if "ping" in message.lower():
             client_socket.sendall(b'+PONG\r\n')
@@ -14,15 +16,14 @@ def handle_connection(client_socket):
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    client_socket, _ = server_socket.accept() # 等待客戶端連接
-
-    threading.Thread(
+    # Uncomment this to pass the first stage
+    while True:
+        
+        client_socket, _ = server_socket.accept() # 等待客戶端連接
+        threading.Thread(
             target=handle_connection, args=[client_socket]
-    ).start()
+        ).start()
 
 
 if __name__ == "__main__":
