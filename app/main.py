08d79e36f1405d9d87035fdf3fe780d91b0c52e1
nissen <nishan.yeh@junyiacademy.org>
2024-05-26 01:05:20 +0800

    pass 3st stage

--- app/main.py
+++ app/main.py
@@ -9,7 +9,13 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    client_socket, _ = server_socket.accept() # 等待客戶端連接
+
+    while True:
+        request: bytes = client_socket.recv(1024) # 獲取客戶端發送的訊息
+        message: str = request.decode()
+        if "ping" in message.lower():
+            client_socket.sendall(b'+PONG\r\n')
 
 
 if __name__ == "__main__":
